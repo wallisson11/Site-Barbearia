b7393d517d3968d54da64df0a37429e759e90b5b
WALLISSON WILLIAM VITOR MARCELINO <wallisson.wvmarcelin@senacsp.edu.br>
2025-05-28 19:37:03 -0300

    barbearia

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -126,10 +126,11 @@ const sendTokenResponse = (user, statusCode, res) => {
 
   const options = {
     expires: new Date(
-      Date.now() + process.env.JWT_COOKIE_EXPIRE * 24 * 60 * 60 * 1000
+      Date.now() + parseInt(process.env.JWT_COOKIE_EXPIRE) * 24 * 60 * 60 * 1000
     ),
     httpOnly: true
   };
+  
 
   if (process.env.NODE_ENV === 'production') {
     options.secure = true;
